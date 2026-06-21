5e5085c11cb7bf88d143d09aaefc735d862c9a0a
zhjie <smthzhj@gmail.com>
2026-06-21 15:08:03 +0800

    allow roon-server unfree package

--- flake.nix
+++ flake.nix
@@ -10,7 +10,12 @@
     { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+          "roon-server"
+        ];
+      };
     in
     {
       packages = {
