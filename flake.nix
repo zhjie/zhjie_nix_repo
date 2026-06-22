8f02f99fe154179545153e893a1b4d0a0313c434
zhjie <smthzhj@gmail.com>
2026-06-22 17:27:42 +0800

    up

--- flake.nix
+++ flake.nix
@@ -12,26 +12,36 @@
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-          "roon-server"
-        ];
-      };
-    in
-    {
-      packages = {
-        claude-code-ide = pkgs.emacsPackages.callPackage ./pkgs/claude-code-ide { };
-        codex-ide = pkgs.emacsPackages.callPackage ./pkgs/codex-ide { };
-        docx = pkgs.callPackage ./pkgs/docx { };
-        ghostel = pkgs.emacsPackages.callPackage ./pkgs/ghostel { };
-        pi-acp = pkgs.callPackage ./pkgs/pi-acp { };
-        pptxgenjs = pkgs.callPackage ./pkgs/pptxgenjs { };
-      } // (if system == "x86_64-linux" then {
-        qterm = pkgs.callPackage ./pkgs/qterm { };
-        roon-server = pkgs.callPackage ./pkgs/roonserver { };
-      } else { });
-    });
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfreePredicate =
+            pkg:
+            builtins.elem (nixpkgs.lib.getName pkg) [
+              "roon-server"
+            ];
+        };
+      in
+      {
+        packages = {
+          claude-code-ide = pkgs.emacsPackages.callPackage ./pkgs/claude-code-ide { };
+          codex-ide = pkgs.emacsPackages.callPackage ./pkgs/codex-ide { };
+          ghostel = pkgs.emacsPackages.callPackage ./pkgs/ghostel { };
+          docx = pkgs.callPackage ./pkgs/docx { };
+          pi-acp = pkgs.callPackage ./pkgs/pi-acp { };
+          pptxgenjs = pkgs.callPackage ./pkgs/pptxgenjs { };
+        }
+        // (
+          if system == "x86_64-linux" then
+            {
+              qterm = pkgs.callPackage ./pkgs/qterm { };
+              roon-server = pkgs.callPackage ./pkgs/roonserver { };
+            }
+          else
+            { }
+        );
+      }
+    );
 }
